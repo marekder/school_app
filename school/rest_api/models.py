9b86132b03a9aab39fc870ab27ca547ca7611d3c
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-03-07 20:53:31 +0100

    migrations

--- school/rest_api/models.py
+++ school/rest_api/models.py
@@ -7,9 +7,15 @@ class Exam(models.Model):
     description = models.CharField(max_length=255)
     final_grade = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     points = models.IntegerField()
     exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
