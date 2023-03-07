5cae6d0ce9e51229bcec28b5396ac60a6e0ef348
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-03-07 20:45:14 +0100

    Exam and Task models

--- school/rest_api/models.py
+++ school/rest_api/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 
 class Exam(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+    final_grade = models.IntegerField()
 
 
 class Task(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    points = models.IntegerField()
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
