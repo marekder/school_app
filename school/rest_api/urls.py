9e84db29230c920e72ad86259bc7c09015391989
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-03-09 21:19:15 +0100

    permissions, NestedDefaultRouter()

--- school/rest_api/urls.py
+++ school/rest_api/urls.py
@@ -6,17 +6,25 @@
 
 from django.urls import path
 from .views import ExamView, TaskView
-
+from rest_framework_extensions.routers import NestedRouterMixin
 from rest_framework.routers import DefaultRouter
+
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
 #     path('api/', include('rest_api.urls'), name='api')
 # ]
 
-router = DefaultRouter()
+class NestedDefaultRouter(NestedRouterMixin, DefaultRouter):
+    pass
 
-router.register('exams', ExamView)
+
+router = NestedDefaultRouter()
+
+# router = DefaultRouter()
+
+router.register('exams', ExamView, basename='exams').register('tasks', TaskView, basename='exams-tasks',
+                                                              parents_query_lookups=['exam'])
 router.register('tasks', TaskView)
 
 urlpatterns = router.urls
--- school/rest_api/views.py
+++ school/rest_api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Exam, Task
 from .serializers import ExamSerializer, TaskSerializer
 from rest_framework.viewsets import ModelViewSet
+from .permissions import IsExamOwnerOrReadOnly, IsOwnerOrReadOnly
+from rest_framework import permissions
 
 
 # from rest_framework.generics import ListCreateAPIView
@@ -10,11 +12,13 @@ from rest_framework.viewsets import ModelViewSet
 class ExamView(ModelViewSet):
     queryset = Exam.objects.all()
     serializer_class = ExamSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 
 class TaskView(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsExamOwnerOrReadOnly)
 
 # class ExamView(ListCreateAPIView):
 #     queryset = Exam.objects.all()
