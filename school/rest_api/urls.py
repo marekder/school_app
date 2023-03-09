243b7770cd4131adf4a52f707c2761661e3360eb
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-03-09 19:33:36 +0100

    Defaultrouter()

--- school/rest_api/urls.py
+++ school/rest_api/urls.py
@@ -7,8 +7,22 @@
 from django.urls import path
 from .views import ExamView, TaskView
 
-urlpatterns = [
-    path("exams/", ExamView.as_view(), name="exams"),
-    path("tasks/", TaskView.as_view(), name="tasks"),
-    # path("exams/<int:id>")
-]
+from rest_framework.routers import DefaultRouter
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include('rest_api.urls'), name='api')
+# ]
+
+router = DefaultRouter()
+
+router.register('exams', ExamView)
+router.register('tasks', TaskView)
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path("exams/", ExamView.as_view(), name="exams"),
+#     path("tasks/", TaskView.as_view(), name="tasks"),
+#     # path("exams/<int:id>")
+# ]
--- school/rest_api/views.py
+++ school/rest_api/views.py
@@ -2,14 +2,25 @@ from django.shortcuts import render
 from .models import Exam, Task
 from .serializers import ExamSerializer, TaskSerializer
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import ListCreateAPIView
 
 
-class ExamView(ListCreateAPIView):
+# from rest_framework.generics import ListCreateAPIView
+
+
+class ExamView(ModelViewSet):
     queryset = Exam.objects.all()
     serializer_class = ExamSerializer
 
 
-class TaskView(ListCreateAPIView):
+class TaskView(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+# class ExamView(ListCreateAPIView):
+#     queryset = Exam.objects.all()
+#     serializer_class = ExamSerializer
+#
+#
+# class TaskView(ListCreateAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
